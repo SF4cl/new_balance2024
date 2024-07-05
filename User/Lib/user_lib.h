fd7aca346d8a77236df97512ce6faad2d96376a5
zancientstory <zancientstory@gmail.com>
2024-07-05 18:04:59 +0800

    删除滤波器

--- User/Lib/user_lib.h
+++ User/Lib/user_lib.h
@@ -89,11 +89,11 @@ extern uint8_t GlobalDebugMode;
 
 typedef struct
 {
-    float input;        //输入数据
-    float out;          //输出数据
-    float min_value;    //限幅最小值
-    float max_value;    //限幅最大值
-    float frame_period; //时间间隔
+    float input;        // 输入数据
+    float out;          // 输出数据
+    float min_value;    // 限幅最小值
+    float max_value;    // 限幅最大值
+    float frame_period; // 时间间隔
 } ramp_function_source_t;
 
 typedef struct __packed
@@ -114,40 +114,41 @@ typedef struct __packed
 
 typedef __PACKED_STRUCT
 {
-    float input;        //输入数据
-    float out;          //滤波输出的数据
-    float num[1];       //滤波参数
-    float frame_period; //滤波的时间间隔 单位 s
-} first_order_filter_type_t;
+    float input;        // 输入数据
+    float out;          // 滤波输出的数据
+    float num[1];       // 滤波参数
+    float frame_period; // 滤波的时间间隔 单位 s
+}
+first_order_filter_type_t;
 
-//快速开方
+// 快速开方
 float Sqrt(float x);
 
-//斜波函数初始化
+// 斜波函数初始化
 void ramp_init(ramp_function_source_t *ramp_source_type, float frame_period, float max, float min);
-//斜波函数计算
+// 斜波函数计算
 float ramp_calc(ramp_function_source_t *ramp_source_type, float input);
 
-//绝对限制
+// 绝对限制
 float abs_limit(float num, float Limit);
-//判断符号位
+// 判断符号位
 float sign(float value);
-//浮点死区
+// 浮点死区
 float float_deadband(float Value, float minValue, float maxValue);
 // int26死区
 int16_t int16_deadline(int16_t Value, int16_t minValue, int16_t maxValue);
-//限幅函数
+// 限幅函数
 float float_constrain(float Value, float minValue, float maxValue);
-//限幅函数
+// 限幅函数
 int16_t int16_constrain(int16_t Value, int16_t minValue, int16_t maxValue);
-//循环限幅函数
+// 循环限幅函数
 float loop_float_constrain(float Input, float minValue, float maxValue);
-//角度 °限幅 180 ~ -180
+// 角度 °限幅 180 ~ -180
 float theta_format(float Ang);
 
 int float_rounding(float raw);
 
-//弧度格式化为-PI~PI
+// 弧度格式化为-PI~PI
 #define rad_format(Ang) loop_float_constrain((Ang), -PI, PI)
 
 void OLS_Init(Ordinary_Least_Squares_t *OLS, uint16_t order);
